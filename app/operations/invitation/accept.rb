65e0f8aa06fe165cda4a5d2661dc781f8ff9f876
Marc Lapierre <marc.lapierre@shopify.com>
2016-09-27 10:57:32 -0400

    Adding invitation accept operation

---
+++ app/operations/invitation/accept.rb
@@ -0,0 +1,23 @@
+class Invitation::Accept < ActiveOperation::Base
+  input :token, accepts: String, type: :keyword, required: true
+  input :user, accepts: User, type: :keyword, required: true
+
+  before do
+    halt unless existing_invitation
+  end
+
+  def execute
+    existing_invitation.update_attributes(
+      user: user,
+      state: 'accepted',
+      accepted_at: Time.now.utc
+    )
+    existing_invitation
+  end
+
+  private
+
+  def existing_invitation
+    @invitation ||= Invitation.pending.where(token: token).first
+  end
+end
---
+++ test/operations/invitation/accept_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class Invitation::AcceptTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:chef)
+    @league = leagues(:super_adventure_club)
+    @invitation = create_invitation(@user.email, @league)
+  end
+
+  test "call succeeds" do
+    op = Invitation::Accept.new(token: @invitation.token, user: @user)
+    op.call
+    assert op.succeeded?
+  end
+
+  test "accepting an invitation changes its state to pending" do
+    invitation = Invitation::Accept.call(token: @invitation.token, user: @user)
+    assert_equal 'accepted', invitation.state
+  end
+
+  test "accepting an invitation sets accepted_on" do
+    Timecop.freeze do
+      invitation = Invitation::Accept.call(token: @invitation.token, user: @user)
+      assert_equal Time.now.utc, invitation.accepted_at
+    end
+  end
+
+  test "accepting an invitation sets the user" do
+    invitation = Invitation::Accept.call(token: @invitation.token, user: @user)
+    assert_equal @user, invitation.user
+  end
+
+  private
+
+  def create_invitation(email, league)
+    Invitation::Create.call(email: email, league: league)
+  end
+end
