4265d09c42568d75d563b9fbff53d1ce3d49f42c
Marc Lapierre <marc.lapierre@shopify.com>
2016-10-17 10:33:06 -0400

    Cleaning up charting code

--- app/assets/javascripts/players.coffee
+++
@@ -1,5 +0,0 @@
-Chart.defaults.global.defaultFontSize = 18;
-Chart.defaults.global.defaultFontFamily = 'Lato';
-# Chart.defaults.global.defaultFontColor = '#e5474b';
-# Chart.defaults.global.defaultFontStyle = 'bold';
-Chart.defaults.global.legend.display = false;
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,6 @@
 class PlayersController < ApplicationController
+  include ChartHelper
+
   before_action :authenticate_user!
   before_action :load_player
   before_action :load_league
@@ -14,6 +16,10 @@ class PlayersController < ApplicationController
 
   private
 
+  def load_player
+    @player = Player.find(params[:id])
+  end
+
   def load_league
     @league = @player.league
     unless @league && current_user.leagues.include?(@league)
@@ -22,41 +28,19 @@ class PlayersController < ApplicationController
     end
   end
 
-  def load_player
-    @player = Player.find(params[:id])
-  end
-
   def rating_histories_chart_data
-    {
-      datasets: [{
-        fill: false,
-        borderColor: "#e5474b",
-        data: get_rating_histories_chart_data
-      }]
-    }
-  end
-
-  def get_rating_histories_chart_data
-    @player.rating_histories.map { |rating_history|
+    return nil unless @player.rating_histories.any?
+    line_chart_data(@player.rating_histories.map { |rating_history|
       {
         x: rating_history.created_at.to_i * 1000,
         y: rating_history.rating_after
       }
-    }
+    })
   end
 
   def rating_histories_chart_options
-    {
-      width: 800,
-      height: 480,
-      scales: {
-        xAxes: [{
-          type: 'time',
-          time: {
-            unit: 'day'
-          }
-        }]
-      }
-    }
+    return nil unless @player.rating_histories.any?
+    time_span = @player.rating_histories.last.created_at - @player.rating_histories.first.created_at
+    line_chart_time_options(time_span)
   end
 end
