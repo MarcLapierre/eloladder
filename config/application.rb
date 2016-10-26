08e2463af1cfe514b4e4a4f720321fe29bf73fa5
Marc Lapierre <marc.lapierre@shopify.com>
2016-10-25 23:17:27 -0400

    Fix winning streak

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require './lib/active_relation_extensions'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
